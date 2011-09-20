8584dbdc467e28d7f9cb8015105a22cd239ccb9f
Marc Fournier <marc.fournier@camptocamp.com>
2011-09-20 12:12:37 +0200

    githubsync: use ssh to checkout github modules instead of anonymous git protocol.

--- manifests/classes/githubsync.pp
+++ manifests/classes/githubsync.pp
@@ -23,6 +23,21 @@ class githubsync {
     home   => "/var/local/run/githubsync",
   }
 
+  file { "/var/local/run/githubsync/.ssh":
+    ensure => directory,
+    mode   => 0700,
+    owner  => "githubsync",
+    group  => "githubsync",
+    require => User["githubsync"],
+  }
+
+  sshkey { "github.com":
+    ensure => present,
+    key    => "AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==",
+    type   => "ssh-rsa",
+    before => File["/usr/local/bin/githubsync-update-status.sh"],
+  }
+
   file { ["/var/local/run/", "/var/local/run/githubsync/"]:
     ensure => directory,
     owner  => "githubsync",
@@ -41,13 +56,17 @@ class githubsync {
 WORKDIR="/var/local/run/githubsync/puppetmaster"
 SOURCE="<%= sourcedir %>"
 
-rm -fr $WORKDIR
-git clone $SOURCE $WORKDIR
+if [ -e "$WORKDIR" ]; then
+  cd "$WORKDIR" && git pull
+else
+  git clone "$SOURCE" "$WORKDIR"
+fi
 
-cd $WORKDIR && \
+cd "$WORKDIR" && \
 for module in $(ls modules); do
-  git remote add --fetch "${module}-on-github" "git://github.com/camptocamp/puppet-${module}.git"
-  git subtree split -P "modules/${module}" --branch "local-${module}"
+  git branch -D "local-${module}"
+  git remote add --fetch "${module}-on-github" "git@github.com:camptocamp/puppet-${module}.git" || git remote update "${module}-on-github"
+  /usr/local/bin/git-subtree split -P "modules/${module}" --branch "local-${module}"
 done
 '),
   }
