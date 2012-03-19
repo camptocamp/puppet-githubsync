826e60858d26835ea82d61b29a613f0d6c66d417
Marc Fournier <marc.fournier@camptocamp.com>
2012-03-19 13:24:54 +0100

    githubsync: access to repos via https instead of ssh.

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,19 +23,8 @@ class githubsync {
     home   => "/var/local/run/githubsync",
   }
 
-  file { "/var/local/run/githubsync/.ssh":
-    ensure => directory,
-    mode   => 0700,
-    owner  => "githubsync",
-    group  => "githubsync",
-    require => User["githubsync"],
-  }
-
   sshkey { "github.com":
-    ensure => present,
-    key    => "AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==",
-    type   => "ssh-rsa",
-    before => File["/usr/local/bin/githubsync-update-status.sh"],
+    ensure => absent,
   }
 
   file { ["/var/local/run/", "/var/local/run/githubsync/"]:
@@ -65,7 +54,7 @@ fi
 cd "$WORKDIR" && \
 for module in $(ls modules); do
   git branch -D "local-${module}"
-  git remote add --fetch "${module}-on-github" "git@github.com:camptocamp/puppet-${module}.git" || git remote update "${module}-on-github"
+  git remote add --fetch "${module}-on-github" "https://github.com/camptocamp/puppet-${module}.git" || git remote update "${module}-on-github"
   /usr/local/bin/git-subtree split -q -P "modules/${module}" --branch "local-${module}"
 done
 
