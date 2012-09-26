56f720bdb2d172ee5aa602f7b5c05b783b4f9a6d
Marc Fournier <marc.fournier@camptocamp.com>
2012-09-26 10:28:09 +0200

    githubsync: rm'd user declaration.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,12 +6,6 @@ class githubsync {
     source => 'puppet:///githubsync/githubsync.sh',
   }
 
-  user { "githubsync":
-    ensure => present,
-    shell  => "/bin/sh",
-    home   => "/var/local/run/githubsync",
-  }
-
   file { ["/var/local/run/", "/var/local/run/githubsync/"]:
     ensure => directory,
     owner  => "githubsync",
@@ -19,7 +13,7 @@ class githubsync {
 
   file { "githubsync.rb":
     ensure => absent,
-    name => $operatingsystem ? {
+    name => $::operatingsystem ? {
       Debian => "/usr/local/lib/site_ruby/1.8/githubsync.rb",
       RedHat => "/usr/lib/ruby/site_ruby/1.8/githubsync.rb",
     },
