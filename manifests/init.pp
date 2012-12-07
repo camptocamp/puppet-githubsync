c130485c8122ee8626cd00758b99d96c3eee9a5f
Mickaël Canévet <mickael.canevet@camptocamp.com>
2012-12-07 11:20:34 +0100

    Lint githubsync::init

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,35 +2,37 @@ class githubsync {
 
   file { '/usr/local/bin/githubsync.sh':
     ensure => present,
-    mode   => 0755,
+    mode   => '0755',
     source => 'puppet:///githubsync/githubsync.sh',
   }
 
-  file { ["/var/local/run/", "/var/local/run/githubsync/"]:
+  file { ['/var/local/run/', '/var/local/run/githubsync/']:
     ensure => directory,
-    owner  => "githubsync",
+    owner  => 'githubsync',
   }
 
-  file { "githubsync.rb":
-    ensure => absent,
-    name => $::operatingsystem ? {
-      Debian => "/usr/local/lib/site_ruby/1.8/githubsync.rb",
-      RedHat => "/usr/lib/ruby/site_ruby/1.8/githubsync.rb",
-    },
+  $file_name = $::operatingsystem ? {
+    Debian => '/usr/local/lib/site_ruby/1.8/githubsync.rb',
+    RedHat => '/usr/lib/ruby/site_ruby/1.8/githubsync.rb',
   }
 
-  file { "/usr/local/bin/githubsync-update-status.sh":
+  file { 'githubsync.rb':
+    ensure => absent,
+    name   => $file_name,
+  }
+
+  file { '/usr/local/bin/githubsync-update-status.sh':
     ensure => absent,
   }
 
-  cron { "update githubsync dashboard":
+  cron { 'update githubsync dashboard':
     ensure => absent,
-    user   => "githubsync",
+    user   => 'githubsync',
   }
 
-  cron { "update githubsync nagios plugin":
+  cron { 'update githubsync nagios plugin':
     ensure => absent,
-    user   => "githubsync",
+    user   => 'githubsync',
   }
 
 }
