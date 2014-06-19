74841195cb59270f95c9cb4252e64f263cee97ab
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-06-19 10:26:19 +0200

    Remove dead code

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,10 +6,6 @@ class githubsync {
     source => 'puppet:///modules/githubsync/githubsync.sh',
   }
 
-  file { '/usr/local/bin/githubsync_gist_json.rb':
-    ensure => absent,
-  }
-
   file { ['/var/local/run/', '/var/local/run/githubsync/']:
     ensure => directory,
     owner  => 'githubsync',
@@ -18,30 +14,6 @@ class githubsync {
   package { 'jgrep':
     ensure   => installed,
     provider => 'gem',
-  }
-
-  $file_name = $::operatingsystem ? {
-    Debian => '/usr/local/lib/site_ruby/1.8/githubsync.rb',
-    RedHat => '/usr/lib/ruby/site_ruby/1.8/githubsync.rb',
-  }
-
-  file { 'githubsync.rb':
-    ensure => absent,
-    name   => $file_name,
-  }
-
-  file { '/usr/local/bin/githubsync-update-status.sh':
-    ensure => absent,
-  }
-
-  cron { 'update githubsync dashboard':
-    ensure => absent,
-    user   => 'githubsync',
-  }
-
-  cron { 'update githubsync nagios plugin':
-    ensure => absent,
-    user   => 'githubsync',
   }
 
 }
